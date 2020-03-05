171cdab2c832351ba9f2e4a6d02fd0ad7a615207
Andreas Koj <kojandreas@gmail.com>
2020-03-05 08:34:19 +0100

    implement minimal functionality of the dashboard

--- public/javascripts/dashboard-script.js
+++ public/javascripts/dashboard-script.js
@@ -1,6 +1,8 @@
 // const
 const clientType = "dashboard";
 
+let photos = ["mountain","lake","family"];
+let currentPhoto = 0;
 
 // run when DOM loaded
 $(document).ready(function () {
@@ -9,5 +11,24 @@ $(document).ready(function () {
         $('#window-status').text(msg.value);
       });
     
+
+      setInterval(function(){
+        $('#displayID').text(photos[currentPhoto]);
+        if (photos.length == currentPhoto + 1){
+          currentPhoto = 0;
+        }
+        else {
+          currentPhoto ++;
+        }
+      }, 3000);
+
+    $("#dark-mode").click(function() {
+      $("#dashboard-container").removeClass('dd').addClass('dl');
+    }); 
+
+    $("#light-mode").click(function() {
+      $("#dashboard-container").removeClass('dl').addClass('dd');
+    });    
+
 
 });
--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -6,11 +6,18 @@ importScripts('lib.js');
 let photos = ["mountain","lake","family"];
 let countPhotos = photos.length;
 
+const clientType = "house";
+const socket = io();
+
+
 postMessage(countPhotos);
 
 onmessage = function(e) {
     let photoName = e.data;
+
     photos.push(photoName);
     countPhotos ++;
+    publish(clientType, {subject: "photo", value: photoName});
     postMessage(countPhotos);
+    
   }
